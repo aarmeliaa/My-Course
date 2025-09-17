a7d0742ceb5c957b37da0f6a3f60b051153c31ae
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-17 14:10:17 +0700

    membuat Tab Layout dengan Fragment dan mengimplementasikan ViewPager2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
